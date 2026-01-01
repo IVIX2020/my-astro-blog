fce22f14030eae58e856a945fb5c691fd1a020b8
IVIX <1610frms2020@gmail.com>
2026-01-01 10:48:21 +0900

    Add GitHub Actions workflow for gh-pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://IVIX2020.github.io',
+	site: 'https://IVIX2020.github.io/my-astro-blog/',
 	integrations: [mdx(), sitemap()],
 });

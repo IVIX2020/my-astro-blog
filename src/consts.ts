7d531b42e25c3ca73ff94e2384b7aa8f5781e1a7
IVIX <1610frms2020@gmail.com>
2026-01-03 06:29:19 +0900

    Deploy to GitHub Pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '1610 web';
+export const SITE_DESCRIPTION = '1610 (紅燈)の雑記・成果物など';
